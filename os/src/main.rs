fa4a5d8fddbee02e8343c6736128b09fbe3a74df
logeable <logeable@gmail.com>
2022-02-16 20:13:07 +0800

    feat: print hello world

--- os/src/main.rs
+++ os/src/main.rs
@@ -2,14 +2,22 @@
 #![no_main]
 
 mod lang_items;
+mod sbi;
 
 use core::arch::global_asm;
+
+use sbi::console_putchar;
 
 global_asm!(include_str!("entry.asm"));
 
 #[no_mangle]
 pub fn rust_main() -> ! {
     clear_bss();
+
+    let str = "hello world\n";
+    for &ele in str.as_bytes() {
+        console_putchar(ele as usize);
+    }
     loop {}
 }
 

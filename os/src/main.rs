50053f7ef1623b3189512ec97a18fd9db896b047
logeable <logeable@gmail.com>
2022-02-17 07:45:54 +0800

    feat: color log section address

--- os/src/main.rs
+++ os/src/main.rs
@@ -5,8 +5,10 @@
 #[macro_use]
 mod console;
 mod lang_items;
+mod log;
 mod sbi;
 
+use crate::sbi::shutdown;
 use core::arch::global_asm;
 
 global_asm!(include_str!("entry.asm"));
@@ -15,8 +17,32 @@ global_asm!(include_str!("entry.asm"));
 pub fn rust_main() -> ! {
     clear_bss();
 
-    println!("hello world");
-    panic!("something wrong!!");
+    extern "C" {
+        fn stext();
+        fn etext();
+        fn srodata();
+        fn erodata();
+        fn sdata();
+        fn edata();
+        fn sbss();
+        fn ebss();
+        fn boot_stack();
+        fn boot_stack_top();
+    }
+    info!("stext: {:#x}", stext as usize);
+    info!("etext: {:#x}", etext as usize);
+    info!("srodata: {:#x}", srodata as usize);
+    info!("erodata: {:#x}", erodata as usize);
+    info!("sdata: {:#x}", sdata as usize);
+    info!("edata: {:#x}", edata as usize);
+    info!("boot_stack: {:#x}", boot_stack as usize);
+    info!("boot_stack_top: {:#x}", boot_stack_top as usize);
+    info!("sbss: {:#x}", sbss as usize);
+    info!("ebss: {:#x}", ebss as usize);
+
+    debug!("hello world");
+    error!("now shutdown");
+    shutdown();
 }
 
 fn clear_bss() {

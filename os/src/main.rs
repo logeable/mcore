f58441aa037e320017675e15a629f47a760522ae
logeable <logeable@gmail.com>
2022-02-16 19:40:26 +0800

    feat: clear .bss

--- os/src/main.rs
+++ os/src/main.rs
@@ -9,5 +9,14 @@ global_asm!(include_str!("entry.asm"));
 
 #[no_mangle]
 pub fn rust_main() -> ! {
+    clear_bss();
     loop {}
+}
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
 }

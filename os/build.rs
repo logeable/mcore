3325758106c0280cf45f4a826dcf76dba019759d
logeable <logeable@gmail.com>
2023-09-19 13:38:41 +0800

    refactor: 消除编译器告警

--- os/build.rs
+++ os/build.rs
@@ -1,4 +1,3 @@
-use std::env;
 use std::fs::{read_dir, File};
 use std::io::{Result, Write};
 
@@ -15,7 +14,6 @@ fn insert_app_data() -> Result<()> {
     let mut f = File::create("src/link_app.S").unwrap();
     let mut apps: Vec<_> = read_dir("../user/src/bin")
         .unwrap()
-        .into_iter()
         .map(|dir_entry| {
             let mut name_with_ext = dir_entry.unwrap().file_name().into_string().unwrap();
             name_with_ext.drain(name_with_ext.find('.').unwrap()..name_with_ext.len());
